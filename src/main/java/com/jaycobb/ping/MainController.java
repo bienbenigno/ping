2457d9493702898385b39829059a66b40bc8fb75
Jaycobb Cruz <jaycobb.cruz@moveassist.com>
2019-12-29 11:58:21 +0800

    logback messaging

--- src/main/java/com/jaycobb/ping/MainController.java
+++ src/main/java/com/jaycobb/ping/MainController.java
@@ -3,6 +3,7 @@ package com.jaycobb.ping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,13 @@ public class MainController {
 
     @GetMapping
     public String get() {
-        log.info("MainController hello");
+        log.info("hello");
         return "hello";
+    }
+
+    @GetMapping(value = "/{message}")
+    public String get(@PathVariable(value = "message") final String message) {
+        log.info("hello " + message);
+        return "hello " + message;
     }
 }
